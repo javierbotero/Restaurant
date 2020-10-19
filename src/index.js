403dc584a5f31ee1febce5033c5eee6277ef83ac
Javier Botero <javierbotero10271@gmail.com>
2020-10-19 13:04:04 -0500

    Add all files to stage area

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './assets/style.css';
-import { greeting, makeClickableMenu } from './greeting';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/js/dist/carousel';
-
-greeting();
-makeClickableMenu();
